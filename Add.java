ea2687dfd19da1d8e86fcdf517df4894795af768
ningjngjing0614 <215937309@qq.com>
2020-12-23 22:04:11 +0800

    宁静静：test

---
+++ Add.java
@@ -0,0 +1,27 @@
+public class Add {//语句中大写开头的字母都是类
+    public static void main(String[] args)
+    {//ctrl+/注释或取消注释
+//        //System.out.println("1+3");//sout
+//        byte byt = 0;
+//        System.out.println(Byte.MIN_VALUE);
+//        System.out.println(Byte.MAX_VALUE);
+//        int num = 0;
+//        System.out.println(Integer.MIN_VALUE);
+//        System.out.println(Integer.MAX_VALUE);
+//        short sho = 0;
+//        System.out.println(Short.MIN_VALUE);
+//        System.out.println(Short.MAX_VALUE);
+//        long lon = 0;
+//        System.out.println(Long.MIN_VALUE);
+//        System.out.println(Long.MAX_VALUE);
+//        byt = 0;
+//        System.out.println(byt);//快捷键：byt.sout
+//        byt = 1;
+//        sho = 9;
+//        char a = 'w';
+//        System.out.println(a);
+        String str = "你好" + "中国";//字符串拼接 str.concat
+        System.out.println(str);
+
+    }
+}
