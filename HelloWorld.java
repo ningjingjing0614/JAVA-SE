9086346c42379ccd6b3cb83fc2c39d7499c32e3a
ningjngjing0614 <215937309@qq.com>
2020-12-23 22:02:17 +0800

    宁静静：test

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {//类的名称建议用英文，英文名首字母最好大写//改类名+文件名用shift+f6
+    public static void main(String[] args) {
+        System.out.println("helloworld");//sout
+        System.out.println("1+3");
+    }
+}
+
