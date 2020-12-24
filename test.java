9d35e8917b933d9d84dcb35e7db8fcc435639f83
ningjngjing0614 <215937309@qq.com>
2020-12-24 22:47:24 +0800

    宁静静：test

---
+++ test.java
@@ -0,0 +1,22 @@
+public class test {
+    public static void main(String[] args){
+//        System.out.println(1+3);
+//        byte a = 12;
+//        System.out.println(a);
+//        System.out.println(Byte.MAX_VALUE);
+//        System.out.println("\\\"hello\\\"");
+//       // System.out.println("\\"hello\\"");
+//        System.out.println("/\"hello/\"");
+//        char c = 'a';
+//        int d = 0123;
+//        int e = 0b1111;
+//        System.out.println(c);
+//        System.out.println(d);
+//        System.out.println(e);
+//        int f = 0x1234;
+//        System.out.println(f);
+        System.out.println(Integer.MAX_VALUE);
+        int a = (int)2147483650L;
+        System.out.println(a);
+    }
+}
