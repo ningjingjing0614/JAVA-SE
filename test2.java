92120dda21379dea935f18e6d6e56aeef0845d3c
ningjngjing0614 <215937309@qq.com>
2020-12-25 22:29:40 +0800

    宁静静：test

---
+++ test2.java
@@ -0,0 +1,81 @@
+public class test2 {
+    public static void main(String[] args) {
+//        byte a = 12;
+//        System.out.println(a);
+//        byte b = (byte)-130;
+//        System.out.println(b);
+//        short c = 200;
+//        int d = (int)4.5;
+//        System.out.println(d);
+//        double e = 234.0000;
+//        System.out.println(e);
+//        int a = 10;
+//        long b = 20;
+//        int c = (int)(a+b);
+//        String str = "wert";
+//        int a = 10;
+//        System.out.println(str + a);
+//        byte a = 120, b = 121;
+//        byte c = (byte) 120 + 4;
+//        System.out.println(c);
+//        byte d = a +c;
+        //int转String
+//        int a = 12;
+//        String b = String.valueOf(a);
+//        System.out.println(a);
+//        byte c = 12;
+//        String d = String.valueOf(c);
+//        System.out.println(d);
+//        char e = 'w';
+//        String f = String.valueOf(e);
+//        System.out.println(f);
+//        boolean g = false;
+//        String h = String.valueOf(g);
+//        System.out.println(h);
+        //String转int
+//        String a = "123";
+//        int b = Integer.parseInt(a);//int ok
+//        System.out.println(b);
+//        String c = "123.3";
+//        double d = Double.parseDouble(c);//double ok
+//        System.out.println(d);
+//        String e = "r";
+//        char f = Character.parseCharacter(e);//char no
+//        System.out.println(Character.MIN_VALUE);
+//        String g = "123";
+//        byte h = Byte.parseByte(g);//byte ok
+//        System.out.println(g);
+//        String i = "123";
+//        long j = Long.parseLong(i);//long ok
+//        System.out.println(j);
+//        String k = "t";
+//        //boolean q = False;
+//        //boolean q = False;//boolean值必须小写
+//        //System.out.println(q);
+//        boolean l = Boolean.parseBoolean(k);//boolean ok
+//        System.out.println(l);
+       // String a = "9";
+//        int b = Integer.parseInt(a,2);
+//        System.out.println(b);
+        String c = "100";
+        int d = Integer.parseInt(c,8);
+        System.out.println(d);
+
+       // int c = Integer.parseInt(a,2);
+        //System.out.println(c);
+        /*int d = Integer.parseInt(a,16);
+        int e = Integer.parseInt(a,10);
+        int f = Integer.parseInt(a,8);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        */
+       // System.out.println(d);
+//        String e = "123";
+//        long f = Long.parseLong(e,8);
+//        String g = "123";
+//        long h = Long.parseLong(e,10);
+//        String e = "123.3";
+//        double f = Double.parseDouble()
+    }
+}
