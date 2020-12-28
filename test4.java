85f1ebee788d728f31cd326643f4a1b710864a17
ningjngjing0614 <215937309@qq.com>
2020-12-28 22:31:15 +0800

    宁静静：test4.java

---
+++ test4.java
@@ -0,0 +1,46 @@
+public class test4 {
+    //\u000A
+//        double num1 = 23.4;
+//        int num2 = 3;
+//        //System.out.println(4/0);
+//        System.out.println(4.0/0);
+//        //System.out.println(4%0);
+//        System.out.println(4.0%0);
+//        double num1 = 3.0;
+//        double num2 = 4.0;
+//        int num3 = 3;
+//        int num4 = 4;
+//        System.out.println(Math.pow(num1, num2));
+//        System.out.println(Math.pow(num3, num4));
+//        System.out.println(Math.pow(num1, num3));
+//        System.out.println(Math.pow(num3, num2));
+//        System.out.println(Math.sqrt(num4));
+//        System.out.println(Math.sqrt(num3));
+//        System.out.println(-7%2);
+//        System.out.println(-7/2);
+    //        System.out.println(Byte.MIN_VALUE);
+//        System.out.println(Byte.MAX_VALUE);
+//        System.out.println(Short.MIN_VALUE);
+//        System.out.println(Short.MAX_VALUE);
+//        System.out.println(Integer.MIN_VALUE);
+//        System.out.println(Integer.MAX_VALUE);
+//        System.out.println(Long.MIN_VALUE);
+//        System.out.println(Long.MAX_VALUE);
+//        System.out.println(Float.MIN_VALUE);
+//        System.out.println(Float.MAX_VALUE);
+//        System.out.println(Double.MIN_VALUE);
+//        System.out.println(Double.MAX_VALUE);
+//        System.out.println(2.0-1.1);
+//        int min = Character.MIN_VALUE;
+//        int max = Character.MAX_VALUE;
+//        System.out.println(min);
+//        System.out.println(max);
+//        System.out.println(Character.MIN_VALUE);
+//        System.out.println(Character.MAX_VALUE);
+    public static void main(String[] args) {
+//        boolean boo = true;
+//        System.out.println(boo+1);
+//        System.out.println(boolean);
+    }
+
+}
