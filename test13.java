72c4c2179ad6ec81642865cf40f959c334a23af2
ningjngjing0614 <215937309@qq.com>
2021-01-17 22:54:09 +0800

    宁静静：test13.java

---
+++ test13.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class test9 {
+//    public static void main(String[] args) {
+//        int[] a = {1, 2, 3, 4};
+//        //int[] b;
+//        //b = { 1,2,3,4 };
+//        int[] b = new int[]{1, 2, 3, 4};
+//        System.out.println(b.length);
+////        b = new int[] { 9,8,7,6,5 };
+//        b = new int[]{9, 8, 7, 6, 5};
+//
+//        System.out.println(b.length);
+//        System.out.println();
+//        for (int i = 0; i < b.length; i++) {
+//            int c = b[i];
+//            System.out.printf("第%d个元素是 %d", i, c);
+//            System.out.println();
+//            System.out.printf("第%d个元素是 %d", i, b[i]);
+//            System.out.println();
+//        }
+//        System.out.println();
+//        System.out.println();
+//        for (int d : b) {
+//            System.out.println(d);
+//        }
+//        System.out.println(b);
+//        String g = Arrays.toString(b);
+//        System.out.println(g);
+//    }
+public static void main(String[] args) {
+    int x = 20;
+    int y= 5;
+    System.out.println(x+y+""+(x+y)+y);
+    System.out.println(x+y+x+""+x+(y+y));
+    System.out.println(x+y+""+x+y+""+y);
+
+}
+}
