67f1a94d27895cd3a9ac8514c9a3ebfc8c251788
ningjngjing0614 <215937309@qq.com>
2021-01-18 19:33:52 +0800

    宁静静：test11.java

---
+++ test11.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+
+public class test11 {
+    //    public static String da(String str) {
+//        String st = "";
+//        if (str == null) {
+//            return null;
+//        }
+//        for (int i = 0; i < str.length(); i++) {
+//            char ch = str.charAt(i);
+//            if (ch >= 65 && ch <= 90) {
+//                ch += 32;
+//            }
+//            st += ch;
+//        }
+//        return st;
+//    }
+//
+//    public static void main(String[] args) {
+//        String str1 = "werYYYY";
+////        da(str);
+//        System.out.println(da(str1));
+//        String str2 = "";
+//        System.out.println(da(str2));
+//    }
+
+////    给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数
+//    public static void rotate(int[] nums, int k) {
+//        int[] num1 = new int[]{};
+//        for (int i = k; i < nums.length; i++) {
+//            for (int j = 0; j < k; j++) {
+//                num1[k] = nums[i];
+//            }
+//        }
+//        for (int n = 0; n < k; n++) {
+//            for (int m = k; m < nums.length; m++) {
+//                nums[m] = nums[n];
+//            }
+//        }
+//        for (int q = 0; q < k; q++) {
+//            for (int p = k; p > 0; p++) {
+//                nums[q] = nums[p];
+//            }
+//        }
+//        System.out.println(Arrays.toString(nums));
+//    }
+//
+//    public static void main(String[] args) {
+//        int[] nums = {1,2,3,4,5,6,7,8,9};
+//        int k = 5;
+//        rotate(nums, k);
+//    }
+
+}
+//        char a = 'A';
+//        int b = a;
+//        System.out.println(b);
+//public static void main(String[] args) {
+//    long[] arr3 = new long[] {1,2,3,4,5};
+//    System.out.println(test14.myString(arr3));
+//}
