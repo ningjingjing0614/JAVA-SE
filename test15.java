cc8ed954f8cf2dd7d8c2c31d5723eb17ea7e6856
ningjngjing0614 <215937309@qq.com>
2021-01-19 20:25:25 +0800

    宁静静：test15.java

---
+++ test15.java
@@ -0,0 +1,15 @@
+public class test15 {
+    public static void main(String[] args) {
+//        long[] arr1 = new long[] {1,2,3,4,5,6};
+//        System.out.println(test14.myString(arr1));
+//        long[] arr2 = new long[] {};
+//        System.out.println(test14.myString(arr2));
+//        long[] arr4 = new long[5];
+//        System.out.println(test14.myString(arr4));
+//        long[] arr5 = new long[] {1,2,3,4,5,6};
+//        long[] arr5 = new long[]{1,2,3,4,5};
+        long[] arr5 = {1,2,3,4,5};
+        long[] arr6 = test14.copyRange(arr5, 1, 1);
+        System.out.println(test14.myString(arr6));
+    }
+}
