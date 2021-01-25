8063a1b08059f88f80655b385583ada501e851db
ningjngjing0614 <215937309@qq.com>
2021-01-25 19:13:34 +0800

    宁静静：test1.java

---
+++ test2.java
@@ -0,0 +1,22 @@
+//public class test2 {
+//    public static String decimal(int n){
+//        //n == 0, return String type"0";
+//        //n != 0,从后往前数每三位加小数点
+//        if(n == 0){
+//            return "0";
+//        }
+//        String tmp = "";
+//        //String ans = String.valueOf(n);
+//
+//        while (n != 0){
+//            int tmp1 = n % 1000;
+//            String ans = String.valueOf(tmp1);
+//            tmp = tmp + "." ;
+//            n = n / 1000;
+//        }
+//        return ans;
+//    }
+//    public static void main(String[] args) {
+//        System.out.println(decimal(1234));
+//    }
+//}
