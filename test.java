9d4947b9b3cbcbfb05bfdc49923ee78703118525
ningjngjing0614 <215937309@qq.com>
2021-01-30 20:22:10 +0800

    宁静静

--- test.java
+++ test.java
@@ -1,22 +1,30 @@
-public class test {
-    public static void main(String[] args){
-//        System.out.println(1+3);
-//        byte a = 12;
-//        System.out.println(a);
-//        System.out.println(Byte.MAX_VALUE);
-//        System.out.println("\\\"hello\\\"");
-//       // System.out.println("\\"hello\\"");
-//        System.out.println("/\"hello/\"");
-//        char c = 'a';
-//        int d = 0123;
-//        int e = 0b1111;
-//        System.out.println(c);
-//        System.out.println(d);
-//        System.out.println(e);
-//        int f = 0x1234;
-//        System.out.println(f);
-        System.out.println(Integer.MAX_VALUE);
-        int a = (int)2147483650L;
-        System.out.println(a);
+package revise;
+
+
+    public class test {
+        public static void main(String[] args) {
+            Prac5 myarray = new Prac5(3);
+            myarray.add(1);
+            myarray.add(2);
+            myarray.add(3);
+            myarray.add(4);
+            myarray.add(4,10);
+            myarray.add(5,30);
+            //myarray.add(5,520);
+            myarray.find(6);
+            myarray.find(10);
+            myarray.remove(2);
+            //myarray.remove(11);
+            myarray.set(2,9);
+            myarray.set(7,9);
+            //myarray.get(3);
+            //myarray.get(6);
+            //myarray.clear();
+            myarray.contains(10);
+            myarray.contains(11);
+            myarray.size();
+            myarray.print();
+            //System.out.println(myarray.size());
+        }
     }
-}
+
