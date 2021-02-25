abf47e71571e9b103d5d27bb2b8cd517675548d9
ningjngjing0614 <215937309@qq.com>
2021-02-25 21:51:02 +0800

    宁静静：testMyDate1.java

---
+++ testMyDate1.java
@@ -0,0 +1,24 @@
+//public class testTeacher3 {
+//    public static void main(String[] args) {
+//        Teacher3 teac1 = new Teacher3("张三",21);
+//        teac1.show();
+//        System.out.println(teac1);
+//        System.out.println(teac1.toString());
+//    }
+//}
+public class testMyDate1 {
+    public static void main(String[] args) {
+        myDate1 from = new myDate1(2020,5,14);
+        myDate1 to = new myDate1(2020,6,14);
+        System.out.println(from);
+        System.out.println(to);
+        System.out.println(to.相差天数(from));
+//        System.out.printf("从%s 到%s 经过了%d天\n",from, to, to.相差天数(from));
+
+//        System.out.println(to.judgeRange(from));
+////        System.out.println(from.judgeRange(to));
+////        System.out.println(to.judgeRange(to));
+////        System.out.println(from.judgeRange(from));
+
+    }
+}
