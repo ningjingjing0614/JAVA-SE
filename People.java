d2f864fe45e4d8e4b6ff5b4ef193af8676dd18cf
ningjngjing0614 <215937309@qq.com>
2021-03-03 22:12:02 +0800

    宁静静：test

---
+++ People.java
@@ -0,0 +1,13 @@
+package pack1;
+
+public class People {
+    String name;
+    int age = 150;
+    String[] ability = {"eat", "walk", "sleep"};
+//    public People(){
+//        System.out.println("i love you");
+//    }
+    public People(String name){
+        this.name = name;
+    }
+}
