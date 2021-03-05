dfced53260f0f0f786fbf941cbfe078ea638bc37
ningjngjing0614 <215937309@qq.com>
2021-03-05 22:39:07 +0800

    宁静静：test

---
+++ People2.java
@@ -0,0 +1,14 @@
+package pack3;
+
+public class People extends Animals {
+    String name = "people";
+//    String skill = "干饭";
+//    String sport = "run";
+    int love = 25;
+    int age = 99;
+
+    public People() {
+//        super();
+        System.out.println("people Here");
+    }
+}
