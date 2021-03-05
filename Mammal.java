3d90bff9415a748fdfd8f396e0f5c5fc1d2d62d6
ningjngjing0614 <215937309@qq.com>
2021-03-05 22:38:14 +0800

    宁静静：test

---
+++ Mammal.java
@@ -0,0 +1,12 @@
+package pack3;
+
+public class Mammal extends Animals{
+    String name = "mammls";
+    String sleep = "night";
+    int age = 100;
+
+    public Mammal() {
+//        super();
+        System.out.println("mammal Here");
+    }
+}
