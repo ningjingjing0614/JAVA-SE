612afe4c3cc6cc091a5ecc2344fd2860204ce8b0
ningjngjing0614 <215937309@qq.com>
2021-03-05 22:37:17 +0800

    宁静静：test

---
+++ prac41.java
@@ -0,0 +1,82 @@
+package pack3;
+
+public class prac40 {
+    public static void main(String[] args) {
+//        Animals ani1 = new Mammal();
+////        System.out.println(anm1);
+//        Animals ani2 = new People();
+////        System.out.println();
+//////        System.out.println(((People) ani2).name);
+///////////
+////        System.out.println(((People) ani2).age);
+////        System.out.println(ani2.age);
+////        System.out.println(((People) ani2).name);
+////        System.out.println(ani2.name);
+////
+////
+//////        System.out.println((People)ani2.good);
+////        System.out.println(ani2.good);
+////
+////        System.out.println(((People) ani2).love);
+//////        System.out.println(ani2.love);
+////
+////        System.out.println(ani2.getClass());
+//
+//
+//
+////        System.out.println(((People) ani2).sport);
+////        System.out.println(((People) ani2).skill);
+////        System.out.println(((People) ani2).sleep);
+////        System.out.println(ani2.sport);
+////        System.out.println(ani2.love);
+////        System.out.println(ani2);
+////        System.out.println(ani2.skill);
+//
+//
+//
+////        System.out.println();
+////        System.out.println(((Mammal) ani1).sleep);
+////        System.out.println(ani1.age);
+////        System.out.println(((Mammal) ani1).age);
+////        System.out.println(ani1.good);
+////        System.out.println(ani1.getClass());
+////        System.out.println(((People)ani2).getClass());
+//////        System.out.println();
+////        System.out.println(ani2 instanceof People);
+////        System.out.println(((People)ani2) instanceof People);
+////
+////        Animals ani3 = new People();
+////        People peo1 = (People)ani3;
+//////        if (ani3 instanceof People){
+////////            People peo1 = (People)ani3;
+//////            System.out.println(peo1 instanceof People);
+//////        }
+////        System.out.println(peo1 instanceof People);
+//
+////        System.out.println(ani2.getClass());
+////
+//        People peo1 = (People)ani2;
+//        System.out.println(peo1.love);
+//        System.out.println(peo1 instanceof People);
+//        int i = 7;
+//        do{
+//            System.out.println(--i);
+//            --i;
+//        }while (i != 0);//每次do循环一次，i减2，永远不能==0
+        String s1 = "asdf";
+        String s2 = "1234";
+        System.out.println(s1+s2);
+        System.out.println(s1);
+        int ssd = 1;
+        System.out.println(ssd);
+        Object o;
+        double i = 9;
+
+
+    }
+//    public static class fff{
+//        public  void ain(String[] args) {
+//
+//        }
+//    }
+}
