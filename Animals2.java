7c8c3ac1f6edda2b4e5e0e386919ca83245a1942
ningjngjing0614 <215937309@qq.com>
2021-03-05 22:37:37 +0800

    宁静静：test

---
+++ Animals2.java
@@ -0,0 +1,12 @@
+package pack3;
+
+public class Animals {
+    String name = "animals";
+//    String eat = "food";
+//    String sport = "walk";
+    int age = 150;
+    int good = 35;
+    public Animals() {
+        System.out.println("animals  Here");
+    }
+}
