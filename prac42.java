16b51f892b79c10385431e2854c055753113a778
ningjngjing0614 <215937309@qq.com>
2021-03-06 22:32:39 +0800

    宁静静：test

---
+++ prac42.java
@@ -0,0 +1,25 @@
+package pack4;
+
+public class prac42 {
+    public static void main(String[] args) {
+//        Animals4 anim1 = new Animals4();
+////        anim1.bbbb();
+//        People4 peop1 = new People4();
+//////        peop1.bbbb();
+////        System.out.println(peop1);
+////        peop1.man();
+////        anim1.man();
+////        float k = 2.4;
+////        int i = k;
+////        System.out.println(i);
+//        Animals4 anim2 = new Animals4();
+//        Animals4 ai2 = new Animals4("ccc");
+//        People4 pas = new People4("Eee");
+//        People4 pas2 = new People4("eee","cdddcc");
+////        System.out.println();
+//        System.out.println(ai2.nema);
+//        System.out.println(pas.nema);
+//        System.out.println(pas2.nema);
+        sout
+    }
+}
