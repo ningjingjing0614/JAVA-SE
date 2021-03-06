cd105a0cec8c8c46a98ed42978f475fc879c2fd7
ningjngjing0614 <215937309@qq.com>
2021-03-06 22:32:24 +0800

    宁静静：test

---
+++ People4.java
@@ -0,0 +1,48 @@
+package pack4;
+
+public class People4 extends Animals4 {
+//    String name = "People";
+//    static int day = 100;
+//
+//    public void ccc(){
+//        System.out.println("cccccc");
+//    }
+//    public static void dddd(){
+//        System.out.println("dddd");
+//    }
+
+//    public void aa(){
+//
+//    }
+
+////    public  void bbbb(){
+//////        super.name;
+////        System. out.println("eeeee");
+//    }
+
+//
+//    public static void man() {
+////        super();
+//        System.out.println("hfff");
+//    }
+//
+//    public People4() {
+//        super();
+////        super(name);
+//    }
+//String nema = "ppp";
+//    public People4() {
+////        super("ddd");
+//        System.out.println("wwwww");
+//    }
+//
+//    public People4(String nema) {
+////        super("uuu");
+//        this.nema = nema;
+//    }
+//
+//    public People4(String nema, String nema1) {
+//        super(nema);
+//        this.nema = nema1;
+//    }
+}
