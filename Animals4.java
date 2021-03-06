d9e2491406e859c51c25f5b00983d81bee2c3575
ningjngjing0614 <215937309@qq.com>
2021-03-06 22:32:10 +0800

    宁静静：test

---
+++ Animals4.java
@@ -0,0 +1,41 @@
+package pack4;
+
+public  class Animals4 {
+//    String name = "Animals";
+//    static int age = 15;
+//
+//    public void aaa(){
+//        System.out.println("aaaaa");
+//    }
+////    public static void bbbb(){
+////        System.out.println("Bbbbb");
+////    }
+//public final void bbbb(){
+//        System.out.println("Bbbbb");
+//    }
+//    public void aa(){
+//        System.out.println("gfd");
+//    }
+//    String name = "ffff";
+//
+//    public Animals4(String name) {
+//        this.name = name;
+//    }
+
+//        public Animals4() {
+//        System.out.println("tttrt");
+//    }
+//String nema = "AAA";
+//public static void man() {
+//    System.out.println("khbj");
+//}
+//
+//    public Animals4() {
+//        System.out.println("tfffsss");
+//    }
+//
+//    public Animals4(String nema) {
+//        this.nema = nema;
+//    }
+
+}
