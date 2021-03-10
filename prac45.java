5b4afd4991d69cb8ee4810d23bf639e009bb9952
ningjngjing0614 <215937309@qq.com>
2021-03-10 22:25:49 +0800

    宁静静：test

---
+++ prac45.java
@@ -0,0 +1,40 @@
+public class prac45 {
+    public static void main(String[] args) {
+        String name = "Animals";
+//    static int age = 15;
+//
+//    public void aaa(){
+//        System.out.println("aaaaa");
+//    }
+////    public static void bbbb(){
+////        System.out.println("Bbbbb");
+////    }
+//public final void bbbb(){
+//        System.out.println("Bbbbb");
+//    }
+//    public void aa(){
+//        System.out.println("gfd");
+//    }
+//    String name = "ffff";
+//
+//    public Animals4(String name) {
+//        this.name = name;
+//    }
+
+//        public Animals4() {
+//        System.out.println("tttrt");
+//    }
+//String nema = "AAA";
+//public static void man() {
+//    System.out.println("khbj");
+//}
+//
+//    public Animals4() {
+//        System.out.println("tfffsss");
+//    }
+//
+//    public Animals4(String nema) {
+//        this.nema = nema;
+//    }
+    }
+}
