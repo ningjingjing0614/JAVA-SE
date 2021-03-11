00c2a865ce09039a7e002c95c1f98b2354f3eb25
ningjngjing0614 <215937309@qq.com>
2021-03-11 22:52:57 +0800

    宁静静：test

---
+++ test28.java
@@ -0,0 +1,12 @@
+package pack4;
+
+public class test28 {
+    public static void main(String[] args) {
+        //根据Animals类创建animals对象
+        Animals5 animals = new Animals5();
+        animals.name = "panda";
+        animals.age = 150;
+        System.out.println(animals.name); //输出panda
+        System.out.println(animals.age); //输出150
+    }
+}
