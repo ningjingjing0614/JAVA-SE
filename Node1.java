161c7604990776b7dafe543420a2d57767e98a30
ningjngjing0614 <215937309@qq.com>
2021-03-12 22:39:20 +0800

    宁静静：test

---
+++ Node1.java
@@ -0,0 +1,19 @@
+package pack5;
+//import pack5.Link1;
+
+public class Node1 {
+    public int val;
+    public Node1 next;
+}
+////class lin{
+//    public static void main(String[] args) {
+//        Node1 head = null;
+//
+//        Node1 node1 = new Node1();
+//        node1.val = 1;
+//        node1.next = head;
+//
+//
+//
+//
+//}
