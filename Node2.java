e0ffcd46f5cbc14f456af876a68a7da09a14e15c
ningjngjing0614 <215937309@qq.com>
2021-03-13 18:37:14 +0800

    宁静静：Test29.java

---
+++ Node2.java
@@ -0,0 +1,25 @@
+package pack5;
+
+public class Node2 {
+    public int val;
+    public Node2 next;
+
+    public Node2(int val) {
+        this.val = val;
+    }
+
+    public Node2(int val, Node2 next) {
+        this.val = val;
+        this.next = next;
+    }
+
+//class Test{
+//        static int a;
+//        int b;
+//
+//    public static void main(String[] args) {
+//        int c;
+//    }
+//}
+
+}
