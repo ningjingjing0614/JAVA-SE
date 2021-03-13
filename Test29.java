0ce3adb0ea6b1f21aec2fdd3a39f749d149c171d
ningjngjing0614 <215937309@qq.com>
2021-03-13 18:36:53 +0800

    宁静静：Test29.java

---
+++ Test29.java
@@ -0,0 +1,15 @@
+package pack5;
+
+public class Test29 {
+
+    static int a;//静态属性/类成员变量/类变量
+
+    int b;//属性/成员变量/实例变量
+
+    public static void main(String[] args) {//形参/局部变量
+
+        int c;//局部变量
+
+    }
+}
+
