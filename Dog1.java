c5b0a18d1be306f165e961ddaa364ba53696f5da
ningjngjing0614 <215937309@qq.com>
2021-03-14 22:14:58 +0800

    宁静静：test

---
+++ Dog1.java
@@ -0,0 +1,44 @@
+//public class prac47 {
+//    class ClassName{
+//        field;//成员属性
+//        method;//成员方法
+//    }
+//class WashingMachine {
+//    String producted;
+//    int size;
+//    String color;
+//
+//    public void wash() {
+//        System.out.println("洗衣机可以洗衣服");
+//    }
+//    public void spinDry() {
+//        System.out.println("洗衣机可以甩干衣服");
+//    }
+//}
+//小狗类：
+//        属性：名字、年龄、性别、颜色、体重……
+//        功能：bark、run、eat……
+//public class Dog1 {
+//    String name;//成员属性
+//    int age;
+//    String gender;
+//
+//    public void bark() {//成员方法
+//        System.out.println("小狗可以狗吠");
+//    }
+//
+//    public void run() {
+//        System.out.println("小狗可以跑");
+//    }
+//}
+//}
+
+public class Dog1 {
+    String name;//成员属性
+    int age;
+
+
+    public void show() {
+        System.out.println("我的名字是" + name + ", 我今年" + age + "岁了。");
+    }
+}
