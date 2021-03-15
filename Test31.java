4b9e5d01a9d8c855a7359eacc63b53597f883c51
ningjngjing0614 <215937309@qq.com>
2021-03-15 22:15:57 +0800

    宁静静：Test31.java

---
+++ Test31.java
@@ -0,0 +1,60 @@
+public class Test31 {
+    public static void main(String[] args) {
+//        Dog.A a = new Dog.A();//静态内部类的使用
+//        a.myPrint();
+    }
+}
+//Dog dog1 = new Dog();
+//    Dog dog2 = new Dog();
+//        dog1.show();
+//        dog2.show();
+//        Dog.kind = "dog";
+//        Dog.show();
+//        dog1.show();
+//        dog1.kind = "lovely dog";
+//        Dog.show();
+//        dog1.show();
+//        Teacher tea = new Teacher();
+//        System.out.println(tea.age);
+//        Dog.kind = "dog";
+//                System.out.println(Dog.kind);
+//                System.out.println(dog1.kind);
+//                dog1.kind = "lovely dog";
+//                System.out.println(Dog.kind);
+//                System.out.println(dog1.kind);
+//                Dog dog2 = new Dog();
+//                System.out.println(Dog.kind);
+//                System.out.println(dog2.kind);
+//
+//Dog dog2 = new Dog();
+//    Dog dog3 = new Dog();
+//        Dog.print();
+//                Dog.print();
+//                dog1.name = "xiaohua";
+//                Dog.print();
+//public class Test31 {
+//    private static int a;
+//    private int b;
+//    static{
+//        Test31.a=3;
+//        System.out.println(a);
+//        Test31 t=new Test31();
+//        t.f();
+//        t.b=1000;
+//        System.out.println(t.b);
+//    }
+//    static{
+//        Test31.a=4;
+//        System.out.println(a);
+//    }
+//    public static void main(String[] args) {
+//// TODO 自动生成方法存根
+//    }
+//    static{
+//        Test31.a=5;
+//        System.out.println(a);
+//    }
+//    public void f(){
+//        System.out.println("hhahhahah");
+//    }
+//}
